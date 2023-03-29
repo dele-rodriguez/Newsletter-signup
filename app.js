21e837e0980375077b9a4fa05f86ec0bede22a08
dele-rodriguez <114116037+dele-rodriguez@users.noreply.github.com>
2023-03-29 17:15:53 +0100

    Api KEy was corrected

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.post("/" , function(req , res) {
     const mailchimp = require("@mailchimp/mailchimp_marketing");
 
     mailchimp.setConfig({
-    apiKey: "0027c6fa91ac418b6f2819c1f1dfc261-us",
+    apiKey: "e17a080ca02459323bb06157c516b3eb-us14",
     server: "us14",
     });
 
@@ -60,50 +60,3 @@ app.post("/failure" , ( req , res ) => {
 app.listen(process.env.PORT || 4000, function() {
     console.log("server is up and running fast!");
 });
-
-
-
-// const response = await mailchimp.ping.get();
-
-    // const response = await client.lists.getAllLists();
-
-// const response = await client.lists.getList("4671d29ab7");
-
-// const data = {
-//     members: [
-//         {
-//             email_address: email,
-//             status: "subscribed",
-//             merge_fields: {
-//                 FNAME: firstName,
-//                 LNAME: lastName
-//             }
-//         }
-//     ]
-// };
-
-// const jsonData = JSON.stringify(data);
-
-// const url = "https://us14.api.mailchimp.com/3.0/lists/4671d29ab7";
-
-// const options = {
-//     method: "POST",
-//     auth: "dele:0027c6fa91ac418b6f2819c1f1dfc261-us14"
-// }
-
-// const request = https.request(url , options , (response) => {
-//     const status = response.statusCode;
-//     if (status === 200) {
-//         console.log("Subscription successful!");
-//     } else {
-//         console.log("You no wise!");
-//     }
-//     console.log("status: " + status);
-//     response.on("data" , function(data) {
-//         console.log(JSON.parse(data));
-//     });
-
-// });
-
-// request.write(jsonData);
-// request.end;
